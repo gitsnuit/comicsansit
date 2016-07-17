ffe89154dc6fb5100abac3a14a5bcf23f7b49399
MartyBum <martyn.alberts.coding@gmail.com>
2016-07-17 22:06:06 +0200

    Initial Commit of the ComicSansIt Plugin

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+// The base URL where the User Manual for Zarafa can be found
+define('PLUGIN_COMICSANSIT_USER_DEFAULT_ENABLE', false);
+?>
---
+++ js/comicsansit.js
@@ -0,0 +1,27 @@
+Ext.namespace('Zarafa.plugins.comicsansit');
+
+/**
+ * @class Zarafa.plugins.comicsansit.ComicSansItPlugin
+ * @extends Zarafa.core.ThemePlugin
+ *
+ */ 
+Zarafa.plugins.comicsansit.ComicSansItPlugin  = Ext.extend(Zarafa.core.Plugin, {
+     
+
+    initPlugin : function() {
+    document.body.className = "comicsansit";
+    
+    } 
+        	    
+});
+
+
+Zarafa.onReady(function() {
+	container.registerPlugin(new Zarafa.core.PluginMetaData({
+		name : 'comicsansit',
+		displayName : _('Comic Sans It'),
+		allowUserDisable : true,
+		allowUserVisible : true,
+		pluginConstructor : Zarafa.plugins.comicsansit.ComicSansItPlugin
+	}));
+});
